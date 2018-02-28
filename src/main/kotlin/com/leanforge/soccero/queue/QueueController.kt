492a6da0e7e09c81e2fdb458cf6569db038aaeaf
Patryk Dunski <patryk.dunski@kende.pl>
2018-02-28 19:15:33 +0000

    Adds commands: queue definition - define private channels

--- src/main/kotlin/com/leanforge/soccero/queue/QueueController.kt
+++ src/main/kotlin/com/leanforge/soccero/queue/QueueController.kt
@@ -1,5 +1,6 @@
 package com.leanforge.soccero.queue
 
+import com.leanforge.game.slack.SlackService
 import com.leanforge.game.slack.listener.SlackController
 import com.leanforge.game.slack.listener.SlackMessageListener
 import com.leanforge.game.slack.listener.SlackMessageRegexGroup
@@ -8,7 +9,7 @@ import com.leanforge.soccero.league.parser.CompetitionParser
 import org.springframework.beans.factory.annotation.Autowired
 
 @SlackController
-class QueueController @Autowired constructor(private val queueService: QueueService) {
+class QueueController @Autowired constructor(private val queueService: QueueService, private val slackService: SlackService) {
 
     @SlackMessageListener("defineQueue ([^\\s]+) <#([^\\|]*)\\|[^>]*> p(\\d{1})")
     fun defineQueue(
@@ -18,6 +19,19 @@ class QueueController @Autowired constructor(private val queueService: QueueServ
 
     ) : SlackReactionResponse {
         val competition = CompetitionParser.parseSingleDefinition(competitionString)
+        queueService.createConfig(competition, destChannel, Integer.parseInt(priority))
+        return SlackReactionResponse("ok_hand")
+    }
+
+    @SlackMessageListener("defineQueue ([^\\s]+) #([^\\s]+) p(\\d{1})")
+    fun definePrivateQueue(
+            @SlackMessageRegexGroup(1) competitionString: String,
+            @SlackMessageRegexGroup(2) destChannelName: String,
+            @SlackMessageRegexGroup(3) priority: String
+
+    ) : SlackReactionResponse {
+        val competition = CompetitionParser.parseSingleDefinition(competitionString)
+        val destChannel = slackService.getChannelId(destChannelName).orElse(null) ?: return SlackReactionResponse("confused")
         queueService.createConfig(competition, destChannel, Integer.parseInt(priority))
         return SlackReactionResponse("ok_hand")
     }
