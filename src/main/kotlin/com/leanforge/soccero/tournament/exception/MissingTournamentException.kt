d4d412dac54b538cbb9f9ffb48584f31b1fb3dc2
Patryk Dunski <patryk.dunski@kende.pl>
2018-03-01 08:26:17 +0000

    Changes error message on missing tournament

--- src/main/kotlin/com/leanforge/soccero/tournament/exception/MissingTournamentException.kt
+++ src/main/kotlin/com/leanforge/soccero/tournament/exception/MissingTournamentException.kt
@@ -5,5 +5,5 @@ import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 class MissingTournamentException(leagueName: String, competition: String) :
-        IllegalStateException(":smoking: :smoking: :smoking:\n" +
-                "Seems like there is no `$competition` in league `$leagueName`. Let's use MySQL to solve that.")
+        IllegalStateException(":smoking: :smoking: :smoking: :smoking: :smoking: :smoking:\n" +
+                "Seems like there is no `$competition` in league `$leagueName`. Please use MySQL to solve that.")
